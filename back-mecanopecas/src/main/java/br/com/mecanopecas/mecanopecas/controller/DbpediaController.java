10d488d23450233c0313ce4fb30013de353d10f9
Iury Gama <iurygam@hotmail.com>
2024-07-09 21:11:33 -0300

    feat: adicionando busca ao dbpedia sobre marcas de carro

---
+++ back-mecanopecas/src/main/java/br/com/mecanopecas/mecanopecas/controller/DbpediaController.java
@@ -0,0 +1,25 @@
+package br.com.mecanopecas.mecanopecas.controller;
+
+import br.com.mecanopecas.mecanopecas.services.DbpediaService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/dbpedia")
+public class DbpediaController extends BaseController {
+
+    private final DbpediaService dbpediaService;
+    public DbpediaController(final DbpediaService dbpediaService, @Qualifier("jwtDecoder") JwtDecoder jwtDecoder) {
+        super(jwtDecoder);
+        this.dbpediaService = dbpediaService;
+    }
+
+    @GetMapping("/marcas")
+    public List<String> consultarMarcas(){
+        return dbpediaService.constultaMarcas();
+    }
+
+}
---
+++ back-mecanopecas/src/main/java/br/com/mecanopecas/mecanopecas/services/DbpediaService.java
@@ -0,0 +1,50 @@
+package br.com.mecanopecas.mecanopecas.services;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DbpediaService {
+
+    private static final String SPARQL_URL = "https://dbpedia.org/sparql";
+    public List<String> constultaMarcas(){
+        List<String> partList = new ArrayList<>();
+        String query = "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?manufacturerLabel\n" +
+                "WHERE {\n" +
+                "  ?car a dbo:Automobile ;\n" +
+                "       rdfs:label ?carLabel ;\n" +
+                "       dbo:manufacturer ?manufacturer .\n" +
+                "  ?manufacturer rdfs:label ?manufacturerLabel .\n" +
+                "  FILTER (lang(?carLabel) = \"pt\")\n" +
+                "  FILTER (lang(?manufacturerLabel) = \"pt\")\n" +
+                "}\n";
+//                    + "LIMIT 100\n";
+
+        try {
+            QueryExecution queryExecution = QueryExecution.service(SPARQL_URL).query(query).build();
+            ResultSet resultSet = queryExecution.execSelect();
+            while(resultSet.hasNext()){
+                QuerySolution querySolution = resultSet.next();
+                String literal = querySolution.getLiteral("manufacturerLabel").getString();
+                partList.add(literal);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return partList;
+    }
+}
